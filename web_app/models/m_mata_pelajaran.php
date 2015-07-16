42a39630e53a8f04cf4cce03681a15897fb98463
dhutapratama <dhutapratama@gmail.com>
2015-07-16 16:05:12 +0700

    Fix #6 added m_mata_pelajaran

---
+++ web_app/models/m_mata_pelajaran.php
@@ -0,0 +1,69 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+	Data ID yang masuk kedalam model ini harus terenskripsi.
+*/
+
+class M_mata_pelajaran extends CI_Model{
+
+	// Retrieve all data from table
+	public function get_mata_pelajaran () {
+		$database = $this->db->select('*')
+					->from('mata_pelajaran')
+					->get()->result();
+		return $database;
+	}
+
+	// Retrieve data by id from table
+	public function get_mata_pelajaran_by_id ($encrypted_id = '') {
+		$id = $this->encrypt->decode($encrypted_id);
+
+		$database = $this->db->select('*')
+					->from('mata_pelajaran')
+					->where('id', $id)
+					->get();
+
+		if ($database->num_rows() > 0) {
+			$database = $database->result();
+			return $database[0];
+		} else {
+			return false;
+		}
+		
+	}
+
+	// Insert data to table
+	public function insert_mata_pelajaran ($data = array()) {
+		/*
+			$data['mapel'] = ;
+		*/
+
+		$database = $this->db->insert('mata_pelajaran', $data);
+		return $database;
+	}
+
+	// Update data to table
+	public function update_mata_pelajaran ($encrypted_id = '', $data = array()) {
+		/*
+			$data['mapel'] = ;
+		*/
+
+		$id = $this->encrypt->decode($encrypted_id);
+		$this->db->where('id', $id);
+		$database = $this->db->update('mata_pelajaran', $data);
+		return $database;
+	}
+
+	// Delete data to table by id
+	public function delete_mata_pelajaran ($encrypted_id = '') {
+		$id = $this->encrypt->decode($encrypted_id);
+		$this->db->where('id', $id);
+		$database = $this->db->delete('mata_pelajaran');
+		return $database;
+	}
+
+	// ------------------------------
+	// Custom Function
+	// ------------------------------
+
+}
